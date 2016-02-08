8cbbfc76d55f9e834a12ac14bcc8939f9e1fda09
l77e <lukeedgar@iNET.local>
2016-02-08 16:29:38 +0000

    moregif

--- JAVASCRIPT.js
+++ JAVASCRIPT.js
@@ -10,7 +10,7 @@ theImages[3] = 'https://i.imgur.com/yF6c6rp.gif'
 theImages[4] = 'https://i.imgur.com/iAoAu2Z.gif'
 theImages[5] = 'https://i.imgur.com/tUksM3k.gif'
 theImages[6] = 'https://i.imgur.com/w7oWe0X.gif'
-theImages[7] = 'https://imgur.com/3EtL1f4.jpg'
+theImages[7] = 'https://i.imgur.com/zNfZwEH.gif'
 // do not edit anything below this line
 
 var i = 0
